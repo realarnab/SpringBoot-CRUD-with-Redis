1b83e95adc35c194748d549cfd86ed5b09610a0d
Arnab <arnabmondal604@gmail.com>
2024-07-18 19:59:48 +0530

    user model created

--- src/main/java/com/api/redis/model/User.java
+++ src/main/java/com/api/redis/model/User.java
@@ -1,2 +1,18 @@
-package com.api.redis.model;public class User {
+package com.api.redis.model;
+
+import lombok.*;
+
+import java.io.Serializable;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class User implements Serializable { //object will be stored in the database in serializable form
+
+    private String userId;
+    private String name;
+    private String phone;
+    private String email;
 }

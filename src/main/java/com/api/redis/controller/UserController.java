3688ca3fe60cde24a0ca51bd12ed1801eaaedfec
Arnab <arnabmondal604@gmail.com>
2024-07-18 22:08:50 +0530

    controller layer formed

--- src/main/java/com/api/redis/controller/UserController.java
+++ src/main/java/com/api/redis/controller/UserController.java
@@ -1,2 +1,10 @@
-package com.api.redis.controller;public class UserController {
+package com.api.redis.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/users")
+public class UserController {
+    
 }

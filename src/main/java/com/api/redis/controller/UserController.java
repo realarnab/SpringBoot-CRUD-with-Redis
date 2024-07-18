1d65dc5f7b016e458cf9a25e4410a06420b285c8
Arnab <arnabmondal604@gmail.com>
2024-07-18 22:37:33 +0530

    CRUD APIs created

--- src/main/java/com/api/redis/controller/UserController.java
+++ src/main/java/com/api/redis/controller/UserController.java
@@ -1,10 +1,36 @@
 package com.api.redis.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.api.redis.dao.UserDao;
+import com.api.redis.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/v1/users")
 public class UserController {
-    
+
+    @Autowired
+    private UserDao userDao;
+
+    @PostMapping
+    public User createUser(@RequestBody User user){
+        return userDao.save(user);
+    }
+
+    @GetMapping("/{userId}")
+    public User getUser(@PathVariable String userId){
+        return userDao.get(userId);
+    }
+
+    @GetMapping
+    public Map<Object, Object> getAll(){
+        return userDao.findAll();
+    }
+
+    @DeleteMapping("/{userId}")
+    public void deleteUser(@PathVariable String userId){
+        userDao.delete(userId);
+    }
 }

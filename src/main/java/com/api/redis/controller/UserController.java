8ed6a9866c2734f1eb460387138786097139eb1e
Arnab <arnabmondal604@gmail.com>
2024-07-18 22:08:13 +0530

    service logic added

---
+++ src/main/java/com/api/redis/controller/UserController.java
@@ -0,0 +1,2 @@
+package com.api.redis.controller;public class UserController {
+}
--- src/main/java/com/api/redis/dao/UserDao.java
+++ src/main/java/com/api/redis/dao/UserDao.java
@@ -1,7 +1,33 @@
 package com.api.redis.dao;
 
+import com.api.redis.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.config.CustomEditorConfigurer;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Repository;
+
+import java.util.Map;
 
 @Repository
 public class UserDao {
+    @Autowired
+    private RedisTemplate<String,Object> redisTemplate;
+    private static final String KEY = "USER";
+
+    public User save(User user){
+        redisTemplate.opsForHash().put(KEY, user.getUserId(), user);
+        return user;
+    }
+
+    public User get(String userId){
+        return (User)redisTemplate.opsForHash().get(KEY,userId);
+    }
+
+    public Map<Object,Object> findAll(){
+        return redisTemplate.opsForHash().entries(KEY);
+    }
+
+    public void delete(String userId){
+        redisTemplate.opsForHash().delete(KEY,userId);
+    }
 }
